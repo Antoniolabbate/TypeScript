aebb037a2f7b145881a352adc11c45007a0700fe
antonio l'abbate <antolabb06@gmail.com>
2023-05-23 18:14:25 +0200

    typescript-5

--- typescript/src/typescript.jsx
+++ typescript/src/typescript.jsx
@@ -1,10 +1,33 @@
-type Country = string;
+/**
+ * START: Follow the instructions below.
+ */
 
-const country1: Country = "Nigeria";
+// Add the `any` type to fix the type errors in the following code.
 
-const country2: Country = "Italy";
+let currency: any = { name: "Indian rupee" };
 
-const country3: Country = "China";
+currency.code = "THB";
+
+currency = "Baht";
+
+// Fix the if statement in this function so the type of `value` is narrowed to `string`.
+
+function countryNameLength(value: unknown) {
+    if (typeof value === "string") {
+        console.log(value.length);
+    }
+}
+
+countryNameLength("United States of America");
+
+// Fix the type error we see when calling this function.
+// Hint: Replace one of the `never` types with a different type.
+
+function throwCountryError(message:'string'): never {
+    throw new Error(`Could not find country: ${message}`);
+}
+
+throwCountryError("Narnia");
 
 // ----
 
