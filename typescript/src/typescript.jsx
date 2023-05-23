9497206817b694fceb34e4a4def81ca1a1eb50a7
antonio l'abbate <antolabb06@gmail.com>
2023-05-23 15:01:31 +0200

    typescript-1

---
+++ typescript/src/typescript.jsx
@@ -0,0 +1,27 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country: String = "Italy";
+
+let year: Number = undefined;
+
+let averageTemperature: Number = 23.6;
+
+let visited: Boolean = true;
+
+let currency: null  = null;
+
+// Change the type annotations on these variables so they are correct.
+
+let population: Number = 47_450_795;
+
+let isSummer: Boolean = false;
+
+let languages: String = "Spanish, English";
+
+// ----
+
+export {};
