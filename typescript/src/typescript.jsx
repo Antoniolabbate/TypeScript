cc8f149c22adcb371dbef620c20a790a76afb19a
antonio l'abbate <antolabb06@gmail.com>
2023-05-23 18:33:04 +0200

    typescript-6

--- typescript/src/typescript.jsx
+++ typescript/src/typescript.jsx
@@ -2,32 +2,30 @@
  * START: Follow the instructions below.
  */
 
-// Add the `any` type to fix the type errors in the following code.
+// Add a type annotation to this variable that describes the object it contains.
 
-let currency: any = { name: "Indian rupee" };
+const countryPopulation: {code: String, population: Number} = {
+    code: "NZ",
+    population: 5_135_300,
+};
 
-currency.code = "THB";
+// Define a `Country` interface that describes the object in this variable.
+// Add the `Country` type as a type annotation for this variable.
 
-currency = "Baht";
+interface country = {
+    name: string,
+    code: string,
+    population: number,
+};
 
-// Fix the if statement in this function so the type of `value` is narrowed to `string`.
+// Define a `Currency` type alias that describes the object in this variable.
+// Add the `Currency` type as a type annotation for this variable.
 
-function countryNameLength(value: unknown) {
-    if (typeof value === "string") {
-        console.log(value.length);
-    }
-}
-
-countryNameLength("United States of America");
-
-// Fix the type error we see when calling this function.
-// Hint: Replace one of the `never` types with a different type.
-
-function throwCountryError(message:'string'): never {
-    throw new Error(`Could not find country: ${message}`);
-}
-
-throwCountryError("Narnia");
+const currencyData: Coutry = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
 
 // ----
 
