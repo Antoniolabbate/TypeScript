461409bf31e297aaca70f9939ec0029384631147
antonio l'abbate <antolabb06@gmail.com>
2023-05-23 17:21:17 +0200

    typescript-4

--- typescript/src/typescript.jsx
+++ typescript/src/typescript.jsx
@@ -1,21 +1,10 @@
-/**
- * START: Follow the instructions below.
- */
+type Country = string;
 
-function getPopulation(): any {
-    return 69_950_850;
-}
+const country1: Country = "Nigeria";
 
-// Add a type assertion after the call to `getPopulation()`.
-// Hint: What's the actual type of the value returned by `getPopulation()`?
+const country2: Country = "Italy";
 
-const population = getPopulation() as number ;
-
-// Now you've added a type assertion, there's a type error in the code below.
-// Remove the code that is incorrect.
-
-
-console.log(population * 2);
+const country3: Country = "China";
 
 // ----
 
