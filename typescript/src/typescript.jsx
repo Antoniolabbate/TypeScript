9977cd07327b0d93378891ddafd15f6b620815a1
antonio l'abbate <antolabb06@gmail.com>
2023-05-24 12:11:55 +0200

    typescript-13

--- typescript/src/typescript.jsx
+++ typescript/src/typescript.jsx
@@ -2,22 +2,27 @@
  * START: Follow the instructions below.
  */
 
-// Prevent the array from being modified.
-// Use the generic interface type `ReadonlyArray`: ReadonlyArray<type>
+// Add a tuple type annotation for this tuple.
+const withcountryPopulation: [string, number];
+
+const countryPopulation: [string, number] = ["China", 1_412_600_000];
+
+// Change the `CountryWithPopulation` type into a tuple type.
+// This should fix the type error on Line 14.
+
+type CountryWithCurrency = [string, string, string];
+
+const countryCurrency: CountryWithCurrency = ["Italy", "Euro", "EUR"];
+
+// Add a tuple type annotation that prevents this tuple from being modified.
+// Use the readonly modifier and the tuple type syntax: readonly [type, type]
 // This will cause a type error. Remove the code that now has a type error.
 
-const languages: ReadonlyArray<string> = ["Mongolian", "French", "Basque", "Thai"];
+const countryLanguage: readonly [string, string] = ["Greece", "Greek"];
 
+// countryLanguage.reverse();  // Type error - Property 'reverse' does not exist on type 'readonly [string, string]'.
 
-console.log(languages);
-
-// Prevent the array from being modified.
-// Use the readonly modifier and the array literal type syntax: readonly type[]
-// This will cause a type error. Remove the code that now has a type error.
-
-const currencies: readonly string[] = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
-
-console.log(currencies);
+console.log(countryLanguage);
 
 // ----
 
