6c40b20ee34ba9531d8ad2213f8e266f1c2d9afa
antonio l'abbate <antolabb06@gmail.com>
2023-05-25 23:38:22 +0200

    typescript-15

--- typescript/src/typescript.jsx
+++ typescript/src/typescript.jsx
@@ -1,27 +1,42 @@
-**
- * START: Follow the instructions below.
- */
 
-const populations = [
-    1_412_600_000,
-    60_317_116,
-    69_950_850,
-    10_678_632,
-];
+class Currency {
+    // Add types for these fields and make them private.
+    private name: string;
+    private code: string;
+    private symbol: string;
 
-// The Array reduce method allows us to generate a single value from
-// an array of values. Below we're using it to sum the values of the
-// `populations` array.
-//
-// The `population` parameter in the callback has an inferred type of `number`.
-// Change the initial value (`false`) to a value with the same type as `population`.
-// This should fix the type error on Line 23.
-//
-// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    // Add types to the constructor parameters.
+    constructor(name: string, code: string, symbol: string) {
+        this.name = name;
+        this.code = code;
+        this.symbol = symbol;
+    }
 
-const sumOfPopulations = populations.reduce((total, population) => {
-    return total + population;
-}, 0); // Change the initial value to 0 instead of false
+    // Methods are public by default.
+    // Add a visibility modifier to make it clear this method is public.
+    // Add a return type for this method.
+    describe(): void {
+        // Replace the `null` values below with the appropriate field values.
+        // Hint: Access field values with: this.fieldName
+        let description = `The ${this.name} currency `;
+        description += `has the code ${this.code} `;
+        description += `and uses the symbol ${this.symbol}.`;
+        console.log(description);
+    }
+}
 
-console.log(sumOfPopulations);
+// ----
+
+const currencyNaira = new Currency("Naira", "NGN", "₦");
+console.log(currencyNaira);
+currencyNaira.describe(); // Call the `describe()` method on the `currencyNaira` object.
+
+// ----
+
+const currencyUsDollar = new Currency("United States dollar", "USD", "$");
+console.log(currencyUsDollar);
+currencyUsDollar.describe(); // Call the `describe()` method on the `currencyUsDollar` object.
+
+// ----
+
 export {};
